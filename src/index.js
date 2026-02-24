18488101e5779fa43e2fde8c624efc911b6837b0
acadryxCoder <acadryx.os@gmail.com>
2026-02-24 07:51:15 +0100

    Add webpack config, src files, and gitignore

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+
+console.log("🚀 Webpack template ready!");
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+    hot: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
